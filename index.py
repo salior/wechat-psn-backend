d829a165491ef03cbf4dedc91bd71aa176a3df29
salior <9704369@qq.com>
2018-10-12 09:48:47 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,16 +1,9 @@
 # -*- coding:utf-8 -*-
 import werobot
-
-#robot = werobot.WeRoBot(token='robot', enable_session=True, session_storage=saekvstorage.SaeKVDBStorage())
-#robot = werobot.WeRoBot(token='testrobot', enable_session=True, session_storage=filestorage.FileStorage())
-robot = werobot.WeRoBot(token='testrobot', enable_session=True)
-
-@robot.subscribe
-def subscribe(message):
-    return '欢迎关注本公众号！'
-
-@robot.handler
+robot = werobot.WeRoBot(token='testrobot')  #token="testrobot"
+@robot.text
 def echo(message):
-    return '我是WeRoBot机器人'
-
-robot.run(server='cherrypy',host='0.0.0.0',port=80)
+    return message.content
+robot.config['HOST'] = '0.0.0.0' #默认允许所有IP访问
+robot.config['PORT'] = 80  #指定80端口
+robot.run()
